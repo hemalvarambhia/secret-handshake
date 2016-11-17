87c518e856347fe70b5bdcb5abcec15c4a440736
Hemal <h.n.varambhia@gmail.com>
2016-11-17 14:08:10 +0000

    Refactor - simplified the code to generate the commands for decimals < 16.

--- secret_handshake.rb
+++ secret_handshake.rb
@@ -11,9 +11,11 @@ class SecretHandshake
       4 => ['close your eyes'],
       8 => ['jump']
     }
-    return [ 'wink', 'double blink' ] if decimal == 3
+    allowed.default = []
+
     return [ 'double blink', 'wink' ] if decimal == 19
 
-    allowed[decimal]
+    0.upto(decimal.bit_length)
+      .inject([]) { |to_run, n| to_run + allowed[decimal[n] * (2 ** n)]}
   end
 end
